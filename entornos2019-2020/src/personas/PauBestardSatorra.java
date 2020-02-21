2f85dd39852a4c0ee1a74f806b6546ea0be8389f
wertons <paubestardsatorra@gmail.com>
2020-02-21 20:44:57 +0100

    Pau Bestard Satorra File update

--- entornos2019-2020/src/personas/PauBestardSatorra.java
+++ entornos2019-2020/src/personas/PauBestardSatorra.java
@@ -1,4 +1,12 @@
 package personas;
+import comun.Persona;
 
-public class PauBestardSatorra {
+
+public class PauBestardSatorra extends Persona {
+    public PauBestardSatorra(String nombre) {
+        super("PauBestardSatorra");
+    }
+    public String llamame(String frase) {
+        return frase.replace('a', 'e');
+    }
 }
