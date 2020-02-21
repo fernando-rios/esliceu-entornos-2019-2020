faf2f39157d493e3ce952f9dea6c172b33b8e6b8
nj_gf <nj_gf@172.16.8.132>
2020-02-21 21:22:23 +0100

    Norbs

---
+++ entornos2019-2020/src/personas/Norberto.java
@@ -0,0 +1,14 @@
+package personas;
+
+public class Norberto extends Persona {
+	public Norberto(String nombre) {
+        super("Norberto");
+	}
+	
+	public String llamame(String frase) {
+		frase =  frase.replace(/[eiou]/gi, 'a');
+		
+		return frase;
+	}
+
+}
