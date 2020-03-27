31062f32c54f152349cf73f1d65b475dd136808c
clorente2001 <clorente2001@gmail.com>
2020-03-27 15:35:36 +0100

    Tests finalizados Carles

--- entornos2019-2020/src/test/CarlesTest.java
+++ entornos2019-2020/src/test/CarlesTest.java
@@ -11,23 +11,46 @@ import org.junit.jupiter.api.Test;
 import personas.Carles;
 
 public class CarlesTest {
+	static String frase = "";
 	
 	@BeforeAll
-	static void setUpBeforeClass() throws Exception {}
+	static void setUpBeforeClass() throws Exception {
+		frase += "Después de BeforeAll";
+		System.out.println(frase);
+	}
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		frase = "Después de BeforeEach";
+		System.out.println(frase);
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception {
+		frase = "Después de AfterEach";
+		System.out.println(frase);
+	}
 
 	@AfterAll
-	static void tearDownAfterClass() throws Exception {}
-
-	@BeforeEach
-	void setUp() throws Exception {}
-
-	@AfterEach
-	void tearDown() throws Exception {}
+	static void tearDownAfterClass() throws Exception {
+		frase = "Despues de AfterAll";
+		System.out.println(frase);
+	}
 	
 	@Test
 	void testLlamame() {
 		Carles c = new Carles("Carles");
-		assertEquals("UEP!", c.llamame("Uep!"));
+		frase = "Hola, primer test";
+		assertEquals(frase, c.llamame("Hola, primer test"));
+		System.out.println(frase);
+	}
+	
+	@Test
+	void testMensaje() {
+		Carles c = new Carles("Carles");
+		frase = "Hola, segon test";
+		assertEquals(frase, c.llamame("Hola, segon test"));
+		System.out.println(frase);
 	}
 
 }
