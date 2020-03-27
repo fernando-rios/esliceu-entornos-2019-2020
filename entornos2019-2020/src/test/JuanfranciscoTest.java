c812e13e8308a3fd32d904b98e479e1739a3a4b5
juanfran <juanfran@192.168.1.113>
2020-03-27 15:35:05 +0100

    Test Hechos

--- entornos2019-2020/src/test/JuanfranciscoTest.java
+++ entornos2019-2020/src/test/JuanfranciscoTest.java
@@ -11,22 +11,55 @@ import org.junit.jupiter.api.Test;
 import personas.Juanfrancisco;
 
 public class JuanfranciscoTest {
+	
+	static String mensaje = "";
 
 	@BeforeAll
-	static void setUpBeforeClass() throws Exception {}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {}
-
-	@BeforeEach
-	void setUp() throws Exception {}
-
-	@AfterEach
-	void tearDown() throws Exception {}
+	static void setUpBeforeClass() throws Exception {
+		mensaje= "Primer paso: BeforeAll";
+		System.out.println(mensaje);
+	}
 	
+	@BeforeEach
+	void setUp() throws Exception {
+		mensaje= "Segundo paso: BeforeEach";
+		System.out.println(mensaje);
+	}
+
 	@Test
 	void testLlamame() {
 		Juanfrancisco j = new Juanfrancisco("Juanfrancisco");
-		assertEquals("PILOTES!", j.llamame("Pilotes!"));
+		mensaje = "Primer test";
+		assertEquals(mensaje, j.llamame("Primer test"));
+		System.out.println(mensaje);
+		}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		mensaje= "Tercer paso: AfterEach";
+		System.out.println(mensaje);
 	}
+	
+	// before each
+	
+	@Test
+	void testLlamame2() {
+		Juanfrancisco j = new Juanfrancisco("Juanfrancisco");
+		mensaje = "Segundo test";
+		assertEquals(mensaje, j.llamame("Segundo test"));
+		System.out.println(mensaje);
+		}
+	
+	//after each
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		mensaje= "Ultimo paso: AfterAll";
+		System.out.println(mensaje);
+	}
+
+
+
+	
+	
 }
